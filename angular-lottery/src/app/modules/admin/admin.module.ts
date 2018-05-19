fdd3753b7062f622e31080cda1ca5aa26f0c78c0
KhaledWielki <adam.rezner95@gmail.com>
2018-05-19 09:22:13 +0200

    Admin Module added

--- angular-lottery/src/app/modules/admin/admin.module.ts
+++ angular-lottery/src/app/modules/admin/admin.module.ts
@@ -5,11 +5,42 @@ import { ZgloszeniDoLoteriiComponent } from './zgloszeni-do-loterii/zgloszeni-do
 import { WynikiLosowaniaComponent } from './wyniki-losowania/wyniki-losowania.component';
 import { HistoriaLosowanComponent } from './historia-losowan/historia-losowan.component';
 import { ZarzadzanieUzytkownikamiComponent } from './zarzadzanie-uzytkownikami/zarzadzanie-uzytkownikami.component';
+import {RouterModule, Routes} from "@angular/router";
 
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    children: [
+      {
+        path: 'historiaLosowan',
+        component: HistoriaLosowanComponent
+      },
+      {
+        path: 'ustawieniaPowiadomien',
+        component: UstawieniaPowiadomienComponent
+      },
+      {
+        path: 'wynikiLosowan',
+        component: WynikiLosowaniaComponent
+      },
+      {
+        path: 'zarzadzanieUzytkownikami',
+        component: ZarzadzanieUzytkownikamiComponent
+      },
+      {
+        path: 'zgloszeniDoLoterii',
+        component: ZgloszeniDoLoteriiComponent
+      }
+    ]
+  }
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
+  exports: [RouterModule],
   declarations: [UstawieniaPowiadomienComponent, ZgloszeniDoLoteriiComponent, WynikiLosowaniaComponent, HistoriaLosowanComponent, ZarzadzanieUzytkownikamiComponent]
 })
 export class AdminModule { }
