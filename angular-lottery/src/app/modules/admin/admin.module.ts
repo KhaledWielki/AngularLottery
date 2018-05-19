427aa80ec44c51be2e3c98f8f9431dffdb020379
grayrattus <arturdziedziczak0@gmail.com>
2018-05-19 11:21:30 +0200

    Some service fix

--- angular-lottery/src/app/modules/admin/admin.module.ts
+++ angular-lottery/src/app/modules/admin/admin.module.ts
@@ -5,7 +5,6 @@ import { WynikiLosowaniaComponent } from './wyniki-losowania/wyniki-losowania.co
 import { HistoriaLosowanComponent } from './historia-losowan/historia-losowan.component';
 import { ZarzadzanieUzytkownikamiComponent } from './zarzadzanie-uzytkownikami/zarzadzanie-uzytkownikami.component';
 import {RouterModule, Routes} from "@angular/router";
-import {ZgloszeniaDoLoteriiService} from "../../services/zgloszenia-do-loterii.service";
 import {ZgloszeniaDoLoteriiComponent} from "./zgloszenia-do-loterii/zgloszenia-do-loterii.component";
 
 
@@ -43,6 +42,6 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
   exports: [RouterModule],
-  declarations: [UstawieniaPowiadomienComponent, ZgloszeniaDoLoteriiService, WynikiLosowaniaComponent, HistoriaLosowanComponent, ZarzadzanieUzytkownikamiComponent]
+  declarations: [UstawieniaPowiadomienComponent,  WynikiLosowaniaComponent, HistoriaLosowanComponent, ZarzadzanieUzytkownikamiComponent, ZgloszeniaDoLoteriiComponent]
 })
 export class AdminModule { }
